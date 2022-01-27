1aa2df7b213b1b82b7f399a7f84aeb13ff9448e9
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-01-27 14:35:44 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :v1 do
+    resources :sessions, only: [:create, :destroy]
+    resources :industries, only: [:index, :show, :create, :destroy] do
+      resources :cars, only: [:show, :create, :destroy] 
+    end
+    resources :users, only: [:create, :show] do
+      resources :appointments, only: [:create, :index]
+    end
+  end
 end
+ 
