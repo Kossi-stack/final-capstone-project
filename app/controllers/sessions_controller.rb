2ed88c2aa1f69fe4d80d3c0a3d1139fe76ba5385
kossi-stack <kossifioklou10@gmail.com>
2022-01-30 00:11:53 -0800

    Update confirmation message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-   
+    render_jsonapi_response(resource)
   end
 
   def respond_to_on_destroy
